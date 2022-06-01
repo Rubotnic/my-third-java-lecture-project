35456e495bcab29a322fabfd933e0a9b0a99a254
Rubotnic <info@rubotnic.com>
2022-06-01 14:06:38 +0200

    My third JAVA lecture project.

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int add1 = 45, add2 = 11;
+        System.out.println("45 + 11 = " + (add1 + add2));
+
+        int mult1 = 12, mult2 = 4;
+        System.out.println("12 * 4 = " + (mult1 * mult2));
+
+        int div1 = 24, div2 = 6;
+        System.out.println("24 / 6 = " + (div1 + div2));
+
+        int sub1 = 55, sub2 = 12;
+        System.out.println("55 - 12 = " + (sub1 + sub2));
+
+    }
+}
